46aa50a3d731b4c438e4c6018f1f31086d6b4ac0
builtpython <petrosyan.manuk@gmail.com>
2020-01-20 18:56:12 +0400

    homework

---
+++ tox.py
@@ -0,0 +1,23 @@
+user_name=input("input your name")
+user_surname=input("input your surname")
+user_age=input("input your age")
+user_profession=input("input your profession")
+
+
+
+print(user_name)
+print(user_surname)
+user_age=int(user_age)
+print(user_age)
+print(user_profession)
+
+
+
+print(type(user_name))
+print(type(user_surname))
+print(type(user_age))
+print(type(user_profession))
+
+  
+
+
