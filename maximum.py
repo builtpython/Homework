d6ed7c23af33300f36ce8d4cef490fc9fd4c99a2
builtpython <petrosyan.manuk@gmail.com>
2020-02-11 21:33:55 +0400

    max of 3 numbers

---
+++ maximum.py
@@ -0,0 +1,14 @@
+num1=5
+num2=17
+num3=11
+
+if(num1>=num2) and (num1>=num3):
+	largest=num1
+elif(num2>=num1) and (num2>=num3):
+	largest=num2
+else: 
+	largest=num3
+print ("The largest number is", largest) 
+
+
+	
