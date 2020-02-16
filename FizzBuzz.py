1588bea5bdab6fa0414ea752c549ae7f4a0a12cb
builtpython <petrosyan.manuk@gmail.com>
2020-02-16 22:14:39 +0400

    Fizz_Buzz 3, 5, 3 and 5

---
+++ FizzBuzz.py
@@ -0,0 +1,24 @@
+
+
+# "2. Write a function called fizz_buzz that takes a number.
+# If the number is divisible by 3, it should return “Fizz”.
+# If it is divisible by 5, it should return “Buzz”.
+# If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+# Otherwise, it should return the same number."
+
+
+
+
+num=int(input("How old are you?"))
+def fizz_buzz(num):
+	if num%3==0 and num%5==0:
+		return"FizzBuzz"
+	elif num%5==0:
+	    return"Buzz"
+	elif num%3==0:
+		return"Fizz"
+	else:
+		return(num)
+print(fizz_buzz(num))	
+
+
