7bad750bcdd964c9cf9fb80782cd4bfb7b6ca34d
builtpython <petrosyan.manuk@gmail.com>
2020-02-16 23:09:14 +0400

    Limit Even Odd

---
+++ Even_Odd.py
@@ -0,0 +1,22 @@
+# "3. Write a function called showNumbers that takes a parameter 
+# called limit. It should print all the numbers between 0 and limit 
+# with a label to identify the even and odd numbers. 
+# For example, if the limit is 3, it should print:
+# 0 EVEN
+# 1 ODD
+# 2 EVEN
+# 3 ODD"
+
+
+
+
+limit=int(input("What is your favorite number?"))
+def showNumbers(limit):
+
+	for i in range(limit+1):
+		if i % 2 == 0:
+			print(str(i), "Even")
+		elif i % 2 != 0:
+			print(str(i), "Odd")
+showNumbers(limit)			
+		
