9a9d340d1186e63e20439139e7a4496bb53deb93
builtpython <petrosyan.manuk@gmail.com>
2020-02-17 00:43:06 +0400

    x(n)=n**2+1

---
+++ squared_function.py
@@ -0,0 +1,10 @@
+# "5. Write a function x(n) for computing an element in the sequence x(n) =n**2 +1
+# Take input from user and print the output
+# Sampe value of n is 4
+# Expected result : 17"
+
+
+n=int(input("Enter some number, por favor"))
+def x(n):
+	return n**2+1
+print(x(n))	
