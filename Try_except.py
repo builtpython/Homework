2bc844226811f992f75eb45df7ce091527b23bcb
builtpython <petrosyan.manuk@gmail.com>
2020-02-18 23:52:11 +0400

    ValueError, try except, else

---
+++ Try_except.py
@@ -0,0 +1,35 @@
+# 3. Learn main python exceptions understand every exceptions meaning.
+#  And rewrite any program from last lessons where can be an error and add try except blocks to fix it.
+
+
+
+
+# while True:        
+#         try:
+#                 num=int(input("What is your phone number? Please write 9 digits using this format: 012345678:"))
+#                 break
+#         except:
+#                 print("Please don't put space between numbers:")
+
+# if str(num).startswith("91") or str(num).startswith("96")  or str(num).startswith("43"):
+#     	print("Your operator is Beeline.")
+# elif str(num).startswith("55") or str(num).startswith("95"):
+#     print("Your operator is Ucom.")
+# elif str(num).startswith("93") or str(num).startswith("94") or str(num).startswith("77"):
+#     print("Your operator is Vivacell.")
+# else: 
+#     print("Is there a new operator that I don't know?")           
+
+
+
+
+
+n=input("Enter your age, por favor")
+# try: 
+# 	age=int(n)
+
+# except ValueError:
+# 	print("Please write in numbers.")
+# else:
+print("You are", n, "years old!")
+
