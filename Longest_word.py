182504c59614e3cf5aca5258190bcef0f4fae2ef
builtpython <petrosyan.manuk@gmail.com>
2020-02-24 00:51:27 +0400

    longest word in list

---
+++ Longest_word.py
@@ -0,0 +1,12 @@
+# 3. Write a python program to find the longest word in the list
+
+
+
+list=['loyer', 'driver', 'sportsmen', 'game']
+
+print(max(list))
+
+
+# my_list = ["apple", "phone", "physics","universe", "joker", "russia"]
+
+# print(max(my_list))
