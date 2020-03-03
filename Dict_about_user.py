7574720dc1812f5401a454a4dde8bc8aa4a96957
builtpython <petrosyan.manuk@gmail.com>
2020-03-04 03:57:33 +0400

    Create a dict from string

---
+++ Dict_about_user.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+my_dict = dict()  
+
+name = input("What is your name? : ")
+age = input("How old are you? :")
+
+key = input("What fruit do you want to buy? : ")
+value = input("How many kilograms? :")
+
+key1 = input("What vegetable do you want to buy? : ")
+value1 = input("How many kilograms? :")
+
+my_dict[name] = age
+my_dict[key] = value
+my_dict[key1] = value1
+print(my_dict) 
