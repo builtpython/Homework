7dc343f6b3e1241392e0b82758b0215776aa9dc7
builtpython <petrosyan.manuk@gmail.com>
2020-03-04 02:28:20 +0400

    Key exsists in a dictionary

---
+++ Exsists_in_dict.py
@@ -0,0 +1,14 @@
+# 3. Write a Python script to check whether a given key already exists in a dictionary.
+
+
+
+dic={"Hayk":30,"Arayik":"Alex","Alen": 10}
+print(dic)
+
+if "Ale" in dic.keys():
+	print("True")
+else:
+    print("False")	
+
+
+
